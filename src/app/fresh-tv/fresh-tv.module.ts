f6c2e1f8259cf2914ac88dfbd93937e35ad13f15
rannar <rannarallorg@gmail.com>
2017-10-19 16:46:54 +0300

    Add toolbar

--- src/app/fresh-tv/fresh-tv.module.ts
+++ src/app/fresh-tv/fresh-tv.module.ts
@@ -1,18 +1,27 @@
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {FreshTvComponent} from './fresh-tv/fresh-tv.component';
 import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     // components
-    FreshTvComponent
+    FreshTvComponent,
+    ToolbarComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    FormsModule,
+    MatInputModule
   ],
   providers: [
     // services
